7ba3fd64c2f8833fece77c5ce791706684f19e79
Deaconescu Horia <deaconescu.horia@gmail.com>
2017-08-03 23:22:29 +0300

    updated footer and header

---
+++ lib/header.php
@@ -0,0 +1,16 @@
+<div class="wrapper row1">
+  <header id="header" class="clear">
+    <div id="hgroup">
+      <h1><a href="#">Roweb</a></h1>
+      <h2>Internship Program</h2>
+    </div>
+    <nav>
+      <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="products.php">Products</a></li>
+        <li><a href="add-product.php">Add product</a></li>
+        <li><a href="login.php">Login</a></li>
+      </ul>
+    </nav>
+  </header>
+</div>
